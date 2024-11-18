fb72357da09001a5f2869f9bbaadbd5e57ab60d1
Martin Mwirigi <mwirigi.8kabush@gmail.com>
2024-11-17 19:24:38 -0800

    0x06. Star Wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,33 +1,17 @@
 #!/usr/bin/node
-const fetch = require('node-fetch');
-const filmID = process.argv[2];
 
-if (!filmID) {
-  console.error('Usage: ./fetch_characters.js <Film ID>');
-  process.exit(1);
-}
+const request = require('request');
 
-async function fetchCharacters (filmId) {
-  const endpoint = `https://swapi.dev/api/films/${filmId}`;
-  try {
-    const response = await fetch(endpoint);
-    if (!response.ok) {
-      throw new Error(`Failed to fetch film: ${response.status}`);
-    }
-    const data = await response.json();
-
-
-    for (const url of data.characters) {
-      const charResponse = await fetch(url);
-      if (!charResponse.ok) {
-        throw new Error(`Failed to fetch character: ${charResponse.status}`);
-      }
-      const charData = await charResponse.json();
-      console.log(charData.name);
-    }
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-  }
-}
-
-fetchCharacters(filmID);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
+  exactOrder(actors, 0);
+});
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, res, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
+  });
+};
